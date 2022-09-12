bd617b05cea0407e853ccee3bc5fd4b9ce4bc1d6
marouf <maroufibrahimi7264@gmail.com>
2022-09-11 20:43:28 -0700

    added active class from header

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 
+function wait() {
+   alert('waiting for future')
+}
+
 // header scrolled
 const header = document.querySelector('header');
 window.addEventListener('scroll', function () {
    header.classList.toggle('sticky', this.window.scrollY > 0);
 })
 
+// header active link
+const link_active = document.querySelectorAll('.link_active');
+function activeLink() {
+   link_active.forEach(e => {
+      e.classList.remove('active');
+   });
+   this.classList.add('active');
+}
+link_active.forEach(e => {
+   e.addEventListener('click', activeLink);
+});
