2fee875904c3fced3e23c9b55a8c5bb1e4637f68
marouf <maroufibrahimi7264@gmail.com>
2022-09-11 17:34:30 -0700

    when scroll the page added a className

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+// header scrolled
+const header = document.querySelector('header');
+window.addEventListener('scroll', function () {
+   header.classList.toggle('sticky', this.window.scrollY > 0);
+})
+
